a39e0f34fe95b69f11d81de5d60e842a8936f263
Ketul Patel <kjpatel2010@gmail.com>
2015-06-25 22:17:30 -0700

    updated localStorage with stringified JSON

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,26 +1,28 @@
-
 window.addEventListener("keydown", function(e) {
     // arrow keys
     if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
         e.preventDefault();
     }
 }, false);
-
+var projectsInfo = JSON.parse(localStorage.getItem("projectsInfo"))
+if(!projectsInfo) {
+  projectsInfo = {}
+}
 
 var main = document.getElementById('main_editor');
 var editor_one = document.getElementById('editor_one');
 var editor_two = document.getElementById('editor_two');
 var editor_three = document.getElementById('editor_three');
 
-if(localStorage.getItem('main_editor')) {
+if(projectsInfo.main_editor) {
 
-  if(localStorage.getItem('main_editor') == "html_editor") {
+  if(projectsInfo.main_editor == "html_editor") {
     main.innerHTML = '<h5>HTML<\/h5><div id="html_editor"><\/div>';
-  } else if(localStorage.getItem('main_editor') == "css_editor") {
+  } else if(projectsInfo.main_editor == "css_editor") {
     main.innerHTML = '<h5>CSS<\/h5><div id="css_editor"><\/div>';
-  } else if(localStorage.getItem('main_editor') == "js_editor") {
+  } else if(projectsInfo.main_editor == "js_editor") {
     main.innerHTML = '<h5>JavaScript<\/h5><div id="js_editor"><\/div><button class="pull-right btn-sm btn-primary" onclick="runScriptButtonPressed()">Evaluate<\/button>';
-  } else if(localStorage.getItem('main_editor') == "terminal") {
+  } else if(projectsInfo.main_editor == "terminal") {
     main.innerHTML = '<h5>Terminal<\/h5><div id="terminal"><\/div>';
   } else {
     main.innerHTML = '<h5>Terminal<\/h5><div id="terminal"><\/div>';
@@ -29,19 +31,18 @@ if(localStorage.getItem('main_editor')) {
 } else {
 
   main.innerHTML = '<h5>Terminal<\/h5><div id="terminal"><\/div>';
-  localStorage.setItem('main_editor', 'terminal');
-
+  projectsInfo.main_editor = 'terminal';
 }
 
-if(localStorage.getItem('editor_one')) {
+if(projectsInfo.editor_one) {
 
-  if(localStorage.getItem('editor_one') == "html_editor") {
+  if(projectsInfo.editor_one == "html_editor") {
     editor_one.innerHTML = '<h5>HTML<\/h5><div id="html_editor"><\/div>';
-  } else if(localStorage.getItem('editor_one') == "css_editor") {
+  } else if(projectsInfo.editor_one == "css_editor") {
     editor_one.innerHTML = '<h5>CSS<\/h5><div id="css_editor"><\/div>';
-  } else if(localStorage.getItem('editor_one') == "js_editor") {
+  } else if(projectsInfo.editor_one == "js_editor") {
     editor_one.innerHTML = '<h5>JavaScript<\/h5><div id="js_editor"><\/div><button class="pull-right btn-sm btn-primary" onclick="runScriptButtonPressed()">Evaluate<\/button>';
-  } else if(localStorage.getItem('editor_one') == "terminal") {
+  } else if(projectsInfo.editor_one == "terminal") {
     editor_one.innerHTML = '<h5>Terminal<\/h5><div id="terminal"><\/div>';
   } else {
     editor_one.innerHTML = '<h5>HTML<\/h5><div id="html_editor"><\/div>';
@@ -49,19 +50,18 @@ if(localStorage.getItem('editor_one')) {
 } else {
 
   editor_one.innerHTML = '<h5>HTML<\/h5><div id="html_editor"><\/div>';
-  localStorage.setItem('editor_one', 'html');
-
+  projectsInfo.editor_one = 'html'
 }
 
-if(localStorage.getItem('editor_two')) {
+if(projectsInfo.editor_two) {
 
-  if(localStorage.getItem('editor_two') == "html_editor") {
+  if(projectsInfo.editor_two == "html_editor") {
     editor_two.innerHTML = '<h5>HTML<\/h5><div id="html_editor"><\/div>';
-  } else if(localStorage.getItem('editor_two') == "css_editor") {
+  } else if(projectsInfo.editor_two == "css_editor") {
     editor_two.innerHTML = '<h5>CSS<\/h5><div id="css_editor"><\/div>';
-  } else if(localStorage.getItem('editor_two') == "js_editor") {
+  } else if(projectsInfo.editor_two == "js_editor") {
     editor_two.innerHTML = '<h5>JavaScript<\/h5><div id="js_editor"><\/div><button class="pull-right btn-sm btn-primary" onclick="runScriptButtonPressed()">Evaluate<\/button>';
-  } else if(localStorage.getItem('editor_two') == "terminal") {
+  } else if(projectsInfo.editor_two == "terminal") {
     editor_two.innerHTML = '<h5>Terminal<\/h5><div id="terminal"><\/div>';
   } else {
     editor_two.innerHTML = '<h5>CSS<\/h5><div id="css_editor"><\/div>';
@@ -70,19 +70,18 @@ if(localStorage.getItem('editor_two')) {
 } else {
 
   editor_two.innerHTML = '<h5>CSS<\/h5><div id="css_editor"><\/div>';
-  localStorage.setItem('editor_two', 'css');
-
+  projectsInfo.editor_two = 'html'
 }
 
-if(localStorage.getItem('editor_three')) {
+if(projectsInfo.editor_three) {
 
-  if(localStorage.getItem('editor_three') == "html_editor") {
+  if(projectsInfo.editor_three == "html_editor") {
     editor_three.innerHTML = '<h5>HTML<\/h5><div id="html_editor"><\/div>';
-  } else if(localStorage.getItem('editor_three') == "css_editor") {
+  } else if(projectsInfo.editor_three == "css_editor") {
     editor_three.innerHTML = '<h5>CSS<\/h5><div id="css_editor"><\/div>';
-  } else if(localStorage.getItem('editor_three') == "js_editor") {
+  } else if(projectsInfo.editor_three == "js_editor") {
     editor_three.innerHTML = '<h5>JavaScript<\/h5><div id="js_editor"><\/div><button class="pull-right btn-sm btn-primary" onclick="runScriptButtonPressed()">Evaluate<\/button>';
-  } else if(localStorage.getItem('editor_three') == "terminal") {
+  } else if(projectsInfo.editor_three == "terminal") {
     editor_three.innerHTML = '<h5>Terminal<\/h5><div id="terminal"><\/div>';
   } else {
     editor_three.innerHTML = '<h5>JavaScript<\/h5><div id="js_editor"><\/div><button class="pull-right btn-sm btn-primary" onclick="runScriptButtonPressed()">Evaluate<\/button>';
@@ -90,11 +89,10 @@ if(localStorage.getItem('editor_three')) {
 } else {
 
   editor_three.innerHTML = '<h5>JavaScript<\/h5><div id="js_editor"><\/div><button class="pull-right btn-sm btn-primary" onclick="runScriptButtonPressed()">Evaluate<\/button>';
-  localStorage.setItem('editor_three', 'js');
-
+  projectsInfo.editor_three = 'js'
 }
 
-main.children[1].style.height = "360px";
+main.children[1].style.height = "340px";
 editor_one.children[1].style.height = "250px";
 editor_two.children[1].style.height = "250px";
 editor_three.children[1].style.height = "250px";
@@ -103,9 +101,9 @@ function goToMain(id) {
   editor_to_main = document.getElementById(id);
   main_to_editor = document.getElementById('main_editor').children[1].id;
 
-  localStorage.setItem(editor_to_main.id, main_to_editor);
-  localStorage.setItem("main_editor", editor_to_main.children[1].id);
-
+  projectsInfo[editor_to_main.id] = main_to_editor;
+  projectsInfo.main_editor = editor_to_main.children[1].id
+  localStorage.setItem("projectsInfo", JSON.stringify(projectsInfo))
   location.reload();
 }
 
@@ -131,7 +129,7 @@ console.log = function() {
 var html_editor = ace.edit("html_editor");
 html_editor.setTheme("ace/theme/monokai");
 html_editor.getSession().setMode("ace/mode/html");
-html_editor.getSession().setValue(localStorage.getItem("html"));
+html_editor.getSession().setValue(projectsInfo.html);
 html_editor.getSession().setUseWrapMode(true);
 
 var html = document.getElementById('html');
@@ -141,7 +139,7 @@ html.innerHTML = html_value;
 var css_editor  = ace.edit("css_editor");
 css_editor.setTheme("ace/theme/github");
 css_editor.getSession().setMode("ace/mode/css");
-css_editor.getSession().setValue(localStorage.getItem("css"));
+css_editor.getSession().setValue(projectsInfo.css);
 css_editor.getSession().setUseWrapMode(true);
 
 var css = document.getElementById('css');
@@ -151,7 +149,7 @@ css.innerHTML = css_value;
 var js_editor = ace.edit("js_editor");
 js_editor.setTheme("ace/theme/solarized_light");
 js_editor.getSession().setMode("ace/mode/javascript");
-js_editor.getSession().setValue(localStorage.getItem("js"));
+js_editor.getSession().setValue(projectsInfo.js);
 js_editor.getSession().setUseWrapMode(true);
 runScript();
 
@@ -159,23 +157,24 @@ html_editor.on('change', function() {
   var html = document.getElementById('html');
   var html_value = html_editor.getSession().getValue();
   html.innerHTML = html_value;
-  localStorage.setItem("html", html_value);
+  projectsInfo.html = html_value
 })
 
 css_editor.on('change', function() {
   var css = document.getElementById('css');
   var css_value = css_editor.getSession().getValue();
   css.innerHTML = css_value;
-  localStorage.setItem("css", css_value);
+  projectsInfo.css = css_value
 })
 
 js_editor.on('change', function() {
   var js_value = js_editor.getSession().getValue();
-  localStorage.setItem("js", js_value);
-  console.dir(localStorage.getItem("js"));
+  console.dir(projectsInfo.js);
+  projectsInfo.js = js_value
 })
 
 function runScriptButtonPressed() {
+  localStorage.setItem("projectsInfo", JSON.stringify(projectsInfo))
   location.reload();
   runScript();
 }
@@ -204,13 +203,13 @@ function constructPage() {
 }
 
 function clearEditor() {
-  localStorage.setItem("project_name", "Project Name");
-  localStorage.setItem("html", "");
-  localStorage.setItem("css", "");
-  localStorage.setItem("js", "");
-  js_editor.getSession().setValue(localStorage.getItem("js"));
-  html_editor.getSession().setValue(localStorage.getItem("html"));
-  css_editor.getSession().setValue(localStorage.getItem("css"));
+  projectsInfo.project_name = "Project Name"
+  projectsInfo.html = ""
+  projectsInfo.css = ""
+  projectsInfo.js = ""
+  js_editor.getSession().setValue(projectsInfo.js);
+  html_editor.getSession().setValue(projectsInfo.html);
+  css_editor.getSession().setValue(projectsInfo.css);
   $('#project_name').val("Project Name");
 }
 
@@ -222,12 +221,12 @@ var alert_messages_block = document.getElementById('alert_messages_block');
 alert_messages_block.innerHTML = "";
 
 $(document).ready(function(){
-  if(localStorage.getItem('project_name')) {
-    $('#project_name').val(localStorage.getItem('project_name'));
+  if(projectsInfo.project_name) {
+    $('#project_name').val(projectsInfo.project_name);
   }
 
   $('#project_name').on('change',function(){
-    localStorage.setItem("project_name", $(this).val());
+    projectsInfo.project_name = $(this).val();
   })
 
   $('#save_project_form').on('submit', function(){
